376486a3f61d338116f57f68724b17ba43139007
Chad Ridings <ChadRidings@users.noreply.github.com>
2025-03-21 09:54:25 -0700

    update tile sizes to use 16:9 ratio

--- vanilla-practice/streaming-guide/src/scripts/constants.js
+++ vanilla-practice/streaming-guide/src/scripts/constants.js
@@ -4,8 +4,8 @@
 
 export const API_URL = 'https://d1q0vy0v52gyjr.cloudfront.net/hub.json';
 export const COLLECTIONS_URL = 'https://d1q0vy0v52gyjr.cloudfront.net/collections/';
-export const SMALL_TILE_JPG = '&size=300x150&format=jpeg';
-export const LARGE_TILE_JPG = '&size=600x300&format=jpeg';
+export const SMALL_TILE_JPG = '&size=300x169&format=jpeg'; // 16:9 ratio
+export const LARGE_TILE_JPG = '&size=600x338&format=jpeg'; // 16:9 ratio
 export const SMALL_WATERMARK = '&size=50x50&format=png';
 export const LARGE_WATERMARK = '&size=100x100&format=png';
 export const HERO_SIZE = '&size=1200x600&format=jpeg';
